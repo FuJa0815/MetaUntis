7cccbd32b3097e399520a27775d12c58c3961998
Jan Führer <jan_fue@icloud.com>
2022-04-23 11:46:33 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import createError from 'http-errors';
+import express, {ErrorRequestHandler} from "express";
+import path from "path";
+import { router as indexRouter } from "./routes";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname);
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+const app = express();
+app.use(connectLivereload());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+}) as ErrorRequestHandler);
+
+module.exports = app;
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+export const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
